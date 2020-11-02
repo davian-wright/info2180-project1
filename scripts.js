7bcce047d8a289430c9e9bd27213f8a6711a7b8d
davian-wright <71860216+davian-wright@users.noreply.github.com>
2020-11-02 01:26:24 -0500

    updated required files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 /* Add your JavaScript to this file */
+
+/*window.onload(var Subbutton=document.getElementsByTagName("button")[0];Subbutton.addEventListener("click",SubbuttonMsg););
+("email").click(function(e)){
+	e.preventDefault();
+}
+
+onclick="document.getElementById('email')
+*/
+    
+document.getElementById("submit").onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("email").innerHTML = alert("Thank you!/*\nYour email addess has been added to our mailing list");
+
+}
